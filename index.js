eb9e16d38d2ac8bc56fcf2c66e48fd9cab838a65
George Harris <citizenharris@gmail.com>
2018-06-05 11:09:42 +0100

    Changed AWS region

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const AWS = require('aws-sdk');
 AWS.config.apiVersions = {
   rekognition: '2016-06-27',
 };
-AWS.config.update({region: process.env.AWS_REGION});
+AWS.config.update({region: 'eu-west-1'}); // Rekognition only works in Ireland
 
 const s3 = new AWS.S3();
 const rekognition = new AWS.Rekognition();
@@ -20,7 +20,7 @@ const getText = (bucket, key) => {
 	    }
 	  }
 	};
-	
+
 	rekognition.detectText(params, function(err, data) {
 	  if (err) console.log(err, err.stack);
 	  else     console.log(data);
