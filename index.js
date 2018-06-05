ca7a00718f03772751de8b954fb96f8e02050cf7
George Harris <citizenharris@gmail.com>
2018-06-05 11:37:21 +0100

    Added regex - only prints number plates now

--- index.js
+++ index.js
@@ -1,14 +1,9 @@
 require('dotenv').config();
-const AWS = require('aws-sdk');
 
-AWS.config.apiVersions = {
-  rekognition: '2016-06-27',
-};
+const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-1'}); // Rekognition only works in Ireland
 
-const s3 = new AWS.S3();
-const rekognition = new AWS.Rekognition();
-
+const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
 
 const getText = (bucket, key) => {
 
@@ -23,7 +18,16 @@ const getText = (bucket, key) => {
 
 	rekognition.detectText(params, function(err, data) {
 	  if (err) console.log(err, err.stack);
-	  else     console.log(data);
+	  else {
+	  	// console.log(data);
+	  	let arrayLength = data.TextDetections.length;
+	  	let regex = /^(([A-Za-z]{1,2}[ ]?[0-9]{1,4})|([A-Za-z]{3}[ ]?[0-9]{1,3})|([0-9]{1,3}[ ]?[A-Za-z]{3})|([0-9]{1,4}[ ]?[A-Za-z]{1,2})|([A-Za-z]{3}[ ]?[0-9]{1,3}[ ]?[A-Za-z])|([A-Za-z][ ]?[0-9]{1,3}[ ]?[A-Za-z]{3})|([A-Za-z]{2}[ ]?[0-9]{2}[ ]?[A-Za-z]{3})|([A-Za-z]{3}[ ]?[0-9]{4}))$/;
+		for (let i = 0; i < arrayLength; i++) {
+			if (data.TextDetections[i].Confidence > 95.0 && data.TextDetections[i].DetectedText.match(regex)) {
+				console.log(data.TextDetections[i]);
+			}
+		}
+	  }
 	});
 };
 
