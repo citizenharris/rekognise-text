1869c966caa70576db1b6469be24d7ba56694593
George Harris <citizenharris@gmail.com>
2018-06-06 19:58:13 +0100

    Returns an array of detected text and some formatting changes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const AWS = require('aws-sdk');
 AWS.config.update({region: 'eu-west-1'}); // Rekognition only works in Ireland
 
-//exports.handler = (event, context, callback) => {
+exports.handler = (event, context, callback) => {
 	const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
 
 	const getText = (bucket, key) => {
@@ -22,14 +22,14 @@ AWS.config.update({region: 'eu-west-1'}); // Rekognition only works in Ireland
 		  	// console.log(data);
 		  	let arrayLength = data.TextDetections.length;
 		  	let regex = /^(([A-Za-z]{1,2}[ ]?[0-9]{1,4})|([A-Za-z]{3}[ ]?[0-9]{1,3})|([0-9]{1,3}[ ]?[A-Za-z]{3})|([0-9]{1,4}[ ]?[A-Za-z]{1,2})|([A-Za-z]{3}[ ]?[0-9]{1,3}[ ]?[A-Za-z])|([A-Za-z][ ]?[0-9]{1,3}[ ]?[A-Za-z]{3})|([A-Za-z]{2}[ ]?[0-9]{2}[ ]?[A-Za-z]{3})|([A-Za-z]{3}[ ]?[0-9]{4}))$/; // Doesn't handle foreign plates
-			for (let i = 0; i < arrayLength; i++) {
-				if (data.TextDetections[i].Confidence > 95.0 && data.TextDetections[i].DetectedText.match(regex)) {
-					console.log(`${data.TextDetections[i].DetectedText} - (Confidence:${data.TextDetections[i].Confidence.toFixed(2)}%)`);
-				}
+		  	let minConfidence = Number(req.minConfidence) || 90.0;
+			let textDetections = [];
+		for (let i = 0; i < arrayLength; i++) {
+			if (data.TextDetections[i].Confidence > minConfidence && data.TextDetections[i].DetectedText.match(regex)) {
+				console.log(`${data.TextDetections[i].DetectedText} - (Confidence: ${data.TextDetections[i].Confidence.toFixed(2)}%)`);
+				textDetections.push(data.TextDetections[i].DetectedText);
 			}
-		  };
-		});
+		}
+		return textDetections;
 	};
-//};
-
-getText(process.env.S3_BUCKET, process.env.S3_EXAMPLE_IMAGE)
+};
