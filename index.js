28db0e428c9b4b5bab0d491f307f0324ea400836
citizenharris <citizenharris@gmail.com>
2018-07-25 15:17:50 +0100

    Removed licence plate regex and changed output structure

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const AWS = require('aws-sdk');
-AWS.config.update({region: 'eu-west-1'}); // Rekognition doesn't work in London
+AWS.config.update({region: 'eu-west-1'}); // Rekognition only works in Ireland
 
 exports.handler = (event, context, callback) => {
 
@@ -7,13 +7,14 @@ exports.handler = (event, context, callback) => {
 	    statusCode: err ? '400' : '200',
 	    body: JSON.stringify(res),
 	    headers: {
-	        'Content-Type': 'application/json',
+					'Content-Type': 'application/json',
 	    }
 	});
 
 	var req = JSON.parse(event.body);
 
 	const rekognition = new AWS.Rekognition({apiVersion: '2016-06-27'});
+
 		var params = {
 		  Image: { 
 		    S3Object: {
@@ -24,22 +25,22 @@ exports.handler = (event, context, callback) => {
 		};
 
 	rekognition.detectText(params, function(err, data) {
-
 	  if (err) console.log(err, err.stack), done(true, {success: false, error: err.stack});
 	  else {
-
 	  	// console.log(data);
 	  	let arrayLength = data.TextDetections.length;
-	  	let regex = /^(([A-Za-z]{1,2}[ ]?[0-9]{1,4})|([A-Za-z]{3}[ ]?[0-9]{1,3})|([0-9]{1,3}[ ]?[A-Za-z]{3})|([0-9]{1,4}[ ]?[A-Za-z]{1,2})|([A-Za-z]{3}[ ]?[0-9]{1,3}[ ]?[A-Za-z])|([A-Za-z][ ]?[0-9]{1,3}[ ]?[A-Za-z]{3})|([A-Za-z]{2}[ ]?[0-9]{2}[ ]?[A-Za-z]{3})|([A-Za-z]{3}[ ]?[0-9]{4}))$/; // Doesn't handle foreign plates
 	  	let minConfidence = Number(req.minConfidence) || 90.0;
 	  	let textDetections = [];
-
-		for (let i = 0; i < arrayLength; i++) {
-			if (data.TextDetections[i].Confidence > minConfidence && data.TextDetections[i].DetectedText.match(regex)) {
-				console.log(`${data.TextDetections[i].DetectedText} - (Confidence: ${data.TextDetections[i].Confidence.toFixed(2)}%)`);
-				textDetections.push(data.TextDetections[i].DetectedText);
+			for (let i = 0; i < arrayLength; i++) {
+				if (data.TextDetections[i].Confidence > minConfidence) {
+				  	let textDetection = {};
+					// console.log(`${data.TextDetections[i].DetectedText} - (Confidence: ${data.TextDetections[i].Confidence.toFixed(2)}%)`);
+					textDetection.DetectedText = data.TextDetections[i].DetectedText;
+					textDetection.Confidence = data.TextDetections[i].Confidence.toFixed(2);
+					textDetections.push(textDetection);
+				}
 			}
-		}
+			// console.log(textDetections);
 		done(null, {success: true, data: textDetections});
 	  }
 	});
